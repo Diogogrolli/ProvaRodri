1d269fe6f5491deef65156f4a4571918f3812104
Diogogrolli <diogoyurigrolli@gmail.com>
2025-06-08 21:48:36 -0300

    Aplicação/App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import RotasAplicacao from './rotas/RotasAplicacao';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <RotasAplicacao />;
 }
 
 export default App;
